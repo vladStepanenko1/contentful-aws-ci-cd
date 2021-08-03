1dad0cc66efe6c31852d2cb250696c45d0aaa699
vladStepanenko1 <vladstepanenko1@gmail.com>
2021-08-04 00:39:53 +0300

    Add CloudFront distribution with Origin access identity for s3 bucket

--- environment/main.go
+++ environment/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/cloudfront"
 	"github.com/pulumi/pulumi-aws/sdk/v4/go/aws/s3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
@@ -29,8 +30,68 @@ func main() {
 			return err
 		}
 
+		// Create OriginAccessIdentity
+		oaiName := "origin-access-identity-s3-bucket"
+		originAccessIdentity, err := cloudfront.NewOriginAccessIdentity(ctx, oaiName, &cloudfront.OriginAccessIdentityArgs{
+			Comment: pulumi.String("Origin access identity for s3 bucket access"),
+		})
+		if err != nil {
+			return err
+		}
+
+		// Create CloudFront distribution
+		cloudfrontDistName := "contentful-app-cdn"
+		bucketOriginId := "contentful-app-bucket-origin"
+		cloudFrontDistribution, err := cloudfront.NewDistribution(ctx, cloudfrontDistName, &cloudfront.DistributionArgs{
+			Enabled: pulumi.Bool(true),
+			Origins: cloudfront.DistributionOriginArray{
+				&cloudfront.DistributionOriginArgs{
+					OriginId:   pulumi.String(bucketOriginId),
+					DomainName: bucket.BucketDomainName,
+					S3OriginConfig: &cloudfront.DistributionOriginS3OriginConfigArgs{
+						OriginAccessIdentity: originAccessIdentity.CloudfrontAccessIdentityPath,
+					},
+				},
+			},
+			DefaultCacheBehavior: &cloudfront.DistributionDefaultCacheBehaviorArgs{
+				AllowedMethods: pulumi.StringArray{
+					pulumi.String("GET"),
+					pulumi.String("HEAD"),
+				},
+				CachedMethods: pulumi.StringArray{
+					pulumi.String("GET"),
+					pulumi.String("HEAD"),
+				},
+				TargetOriginId: pulumi.String(bucketOriginId),
+				ForwardedValues: &cloudfront.DistributionDefaultCacheBehaviorForwardedValuesArgs{
+					QueryString: pulumi.Bool(false),
+					Cookies: &cloudfront.DistributionDefaultCacheBehaviorForwardedValuesCookiesArgs{
+						Forward: pulumi.String("none"),
+					},
+				},
+				ViewerProtocolPolicy: pulumi.String("allow-all"),
+			},
+			Restrictions: &cloudfront.DistributionRestrictionsArgs{
+				GeoRestriction: cloudfront.DistributionRestrictionsGeoRestrictionArgs{
+					RestrictionType: pulumi.String("none"),
+				},
+			},
+			ViewerCertificate: cloudfront.DistributionViewerCertificateArgs{
+				CloudfrontDefaultCertificate: pulumi.Bool(true),
+			},
+		})
+		if err != nil {
+			return err
+		}
+
 		// Export the name of the bucket
 		ctx.Export("bucketName", bucket.ID())
+
+		// Export the CloudFront distribution ID
+		ctx.Export("cloudFrontDistributionId", cloudFrontDistribution.ID())
+
+		// Export CloudFront domain name
+		ctx.Export("cloudFrontDistributionDomainName", cloudFrontDistribution.DomainName)
 		return nil
 	})
 }
